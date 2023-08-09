f08433f56042d89c936c4d22b9d77fc8d4e3e499
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-09 13:07:04 -0500

    Update 3.0_Jedi_Training.py

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -17,6 +17,52 @@
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next. (1pt)
 
+'''
+6. TEMPERATURE PROGRAM (5pts)
+-------------------
+Create a program that asks the user for a temperature in Fahrenheit, and then prints the temperature in Celsius.
+
+Test your program with the following data:
+
+In: 32  Out: 0
+In: 212  Out: 100
+In: 52  Out: 11.1
+In: 25  Out: -3.9
+In: -40  Out: ??? Please tell me what this output is!
+
+'''
+
+
+'''
+7. TRAPEZOID PROGRAM (5pts)
+-------------------
+Create a new program that will ask the user for the information needed to find the area of a trapezoid, and then print the area.
+
+Test with the following:
+
+base 1: 2       base 2: 3    height: 4    area: 10
+base 1: 5       base 2: 7    height: 2    area: 12
+base 1: 1       base 2: 2    height: 3    area: 4.5
+base 1: 7       base 2: 2    height: 4    area: 18
+'''
+
+
+'''
+8. GRADING PROGRAM (5pts)
+---------------
+Create a program that asks the user for their semester grade, final exam grade, 
+and final exam worth and then calculates the overall final grade. 
+Ask your instructor if you don't know how to calculate weighted averages.
+You don't have to print out the letter grade. We will do that in the next chapter.
+
+Test with the following:
+
+Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
+Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
+Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
+'''
+
+
 
 
 
