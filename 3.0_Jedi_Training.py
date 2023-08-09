25894da93fca851a0ded15d3f35ff05d43b463a7
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-09 13:01:29 -0500

    Update 3.0_Jedi_Training.py

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -2,20 +2,22 @@
 # In all the short programs below, do a good job communicating with your end user!
 
 
-# 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
+# 1.) Write a program that asks someone for their name and then prints a greeting that uses their name. (1pt)
 
 
-# 2. Write a program where a user enters a base and height and you print the area of a triangle.
+# 2. Write a program where a user enters a base and height and you print the area of a triangle. (1pt)
 
 
-# 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
+# 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference. (1pt)
 
 
-# 4. Ask a user for an integer and then print the square root.
+# 4. Ask a user for an integer and then print the square root. (1pt)
 
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
-#    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
+#    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next. (1pt)
+
+
 
 
 
