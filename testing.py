de3ccea1dcb623ae5ed36e033dd6fcc0ff2efc83
ngotom <ngotom@urbandaleschools.com>
2024-05-04 21:34:09 -0500

    first commit

--- testing.py
+++ testing.py
@@ -20,5 +20,24 @@
 # mpg = miles_drive/gallons_used
 # print(mpg)
 
-print("Hello my name is \rTommy")
-print("\nHello my name is Tommy")
+# print("Hello my name is \rTommy")
+# print("\nHello my name is Tommy")
+
+n = 6
+stop = 6
+def divisor_sum(n):
+    total = 0
+    for divisor in range(1, n):
+        if n % divisor == 0:
+            total += divisor
+    return total
+
+def is_perfect(n):
+    return n != 0 and n == divisor_sum(n)
+
+def find_perfects(stop):
+    for num in range(1, stop):
+        if is_perfect(num):
+            print("Found perfect number: ", num)
+        if num % 10000 == 0: print('.', end='',flush=True) # progress bar
+    print("Done searching up to ", stop)
